511ff5b9916f9454dd97889ef84a91d8c66f4008
hbpclient <hbpclient@gmx.de>
2021-04-15 18:10:44 +0200

    compontents for drag and drop

---
+++ Solid Twitch Bot Server/Data/TextParameterModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Solid_Twitch_Bot_Server.Data
+{
+    public class TextParameterModel
+    {
+        public string Value { get; set; }
+        //TODO: Backgroundcolor for every type
+    }
+}
